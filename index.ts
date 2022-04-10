90f05af507ef0a037b0f3cb7428f671430c9a73f
jasoncheng <shenjingwa444@qq.com>
2022-04-10 18:45:39 +0800

    匹配 public 中的任意文件

--- index.ts
+++ index.ts
@@ -5,34 +5,19 @@ import * as fs from 'fs';
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, 'public');  // __dirname 表示当前文件所在目录
 server.on('request', (request, response) => {
-  const {method, url:path, headers} = request;
-  const {pathname,search} = new URL(path,'http://localhost:8888/')
-  switch (pathname) {
-    case '/index.html':
-      response.setHeader('Content-Type', 'text/html;charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case '/style.css':
-      response.setHeader('Content-Type', 'text/css;charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case '/main.js':
-      response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    default:
+  const {method, url: path, headers} = request;
+  const {pathname, search} = new URL(path, 'http://localhost:8888/');
+
+  //response.setHeader('Content-Type', 'text/html;charset=utf-8');
+  //pathname: /index.html
+  fs.readFile(p.resolve(publicDir, pathname.substring(1)), (error, data) => {
+    if (error) {
       response.statusCode = 404;
       response.end();
-  }
+    } else {
+      response.end(data.toString());
+    }
+  });
 });
 
 server.listen(8888);
