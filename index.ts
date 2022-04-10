8f203db4979112d398c434873dc69db40148c3d5
jasoncheng <shenjingwa444@qq.com>
2022-04-10 17:24:08 +0800

    根据 url 返回不同的文件

--- index.ts
+++ index.ts
@@ -1,27 +1,34 @@
 import * as http from 'http';
+import * as p from 'path';
+import * as fs from 'fs';
 
 const server = http.createServer();
-
+const publicDir = p.resolve(__dirname, 'public');  // __dirname 表示当前文件所在目录
 server.on('request', (request, response) => {
-  console.log(request.method);
-  console.log(request.url);
-  console.log(request.headers);
-  const array = [];
-  request.on('data', (chunk) => {
-    array.push(chunk);
-  });
-  request.on('end', () => {
-    const body = Buffer.concat(array).toString();
-    console.log('body');
-    console.log(body);
-
-    response.setHeader('x-jason','little funny')
-    console.log(response.getHeader('x-jason'));
-    response.statusCode = 404
-    response.write('1')
-    response.write('23')
-    response.end();
-  });
+  const {method, url, headers} = request;
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type','text/html;charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/style.css':
+      response.setHeader('Content-Type','text/css;charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/main.js':
+      response.setHeader('Content-Type','text/javascript;charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+  }
 });
 
 server.listen(8888);
